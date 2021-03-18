eb20e45ca378757e8990459c10c0abd957641e28
jacktrane <jacktrane@163.com>
2021-03-18 11:30:06 +0800

    time_cost init

--- time_cost/time_cost.go
+++ time_cost/time_cost.go
@@ -1,8 +1,16 @@
 package time_cost
 
-import "sync"
+import (
+		"sync"
+		"time"
+		"strconv"
+		"errors"
+		"fmt"
+		"strings"
+)
 
 var (
+	NS = "ns"
 	MMS = "mms"
 	MS  = "ms"
 	S   = "s"
@@ -13,27 +21,65 @@ type TimeCost struct {
 	unit          string
 	paths         []string
 	pathTimePoint sync.Map
+	t 			  time.Time
 }
 
 func NewTimeCost() *TimeCost {
 	return &TimeCost{
 		unit: MS,
+		paths: make([]string, 0, 10),
+		t: time.Now(),
 	}
 }
 
 func (tc *TimeCost) SetUnit(unit string) error {
+	if len(tc.paths) != 0 {
+		return errors.New("timecost has point")
+	}
 	tc.unit = unit
 	return nil
 }
 
-// TODO 打点
+// 打点
 func (tc *TimeCost) AddPoint(pointName string) {
-
+	tc.paths = append(tc.paths, pointName)
+	tc.pathTimePoint.Store(pointName+strconv.Itoa(len(tc.paths)), time.Since(tc.t))
+	tc.t = time.Now()
 }
 
-// TODO 输出链路
+func (tc *TimeCost) outputCost(d time.Duration) int64 {
+	switch tc.unit {
+		case NS:
+			return d.Nanoseconds()
+		case MMS:
+			return d.Microseconds()
+		case MS:
+			return d.Milliseconds()
+		case S:
+			return int64(d.Seconds())
+		case M:
+			return int64(d.Minutes())
+		default:
+			return int64(d.Milliseconds())
+	}	
+}
+
+// 输出链路
 func (tc *TimeCost) OutputCostStack() string {
-	return ""
+	var totalCost time.Duration
+	arrPointCost := make([]string, len(tc.paths)+2)
+	arrPointCost[0] = "Begin"
+	for index, pointName := range tc.paths {
+		duration, ok := tc.pathTimePoint.Load(pointName)
+		if ok {
+			timeDuration := duration.(time.Duration)
+			totalCost += timeDuration
+			arrPointCost[index+1] = fmt.Sprintf("(%s %d%s)", pointName, tc.outputCost(timeDuration), tc.unit)
+		}
+	}
+
+	arrPointCost[len(tc.paths)+2-1] = "End"
+    return "TotalCost:" + strconv.Itoa(int(tc.outputCost(totalCost))) + ";Detail:" + strings.Join(arrPointCost, "=>")
 }
 
 // TODO 输出两段路径之间的耗时
